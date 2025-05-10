df8e1c1e2eba3cc681ab3ed0991a95a64b164e64
Sinan <sinan_ceviker@yahoo.com>
2025-05-10 20:52:58 +0200

    also dataset handler

---
+++ recaptcha_classifier/data/dataset.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from typing import List, Tuple, Optional, Any
+from torch.utils.data import Dataset
+from .preprocessor import Preprocessor
+from .augment import AugmentationPipeline
+
+
+class DatasetHandler(Dataset):
+    """
+    A class to handle the pairs of (image, label) for the dataset.
+    It makes them ready for training, by applying augmentation
+    for the training set, preprocessing and makes sure that the
+    output format is in PyTorch Tensor format.
+
+    It has the required methods to be used as a inherited class
+    of Dataset, as per the PyTorch documentation: (for custom datasets)
+    https://docs.pytorch.org/tutorials/beginner/basics/data_tutorial.html
+    """
+    def __init__(self,
+                 pairs: List[Tuple[Path, Path]],
+                 preprocessor: Preprocessor,
+                 augmentator: Optional[AugmentationPipeline] = None,
+                 class_map: Optional[dict] = None
+                 ) -> None:
+        """
+        Initializes the DatasetHandler with the given parameters.
+        """
+        self._pairs = pairs
+        self._prep = preprocessor
+        self._aug = augmentator
+        self._class_map = class_map
+
+    def __len__(self) -> int:
+        return len(self._pairs)
+
+    def __getitem__(self,
+                    idx: int
+                    ) -> Tuple[Any, List[Tuple], int]:
+        """
+        Returns the item at the given index, ready for training.
+
+        Args:
+            idx (int): Index of the item to retrieve.
+
+        Returns:
+            Tuple[Any, List[Tuple], int]: A tuple containing the
+            preprocessed image in tensor format, the YOLO bound
+            box annotations and the label.
+        """
+        img_path, lbl_path = self._pairs[idx]
+
+        # Load image and label
+
+        # Augmentation if passed (for training)
+
+        # Bounding Box Scaling if augmentation happens
+
+        # Preprocess image and label
+
+        # Convert label to class index
+        # Return image tensor, bounding box and class index
