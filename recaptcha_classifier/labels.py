1f22021ebe5707bab1b99d8f769ef5c532512973
Sinan <sinan_ceviker@yahoo.com>
2025-05-10 20:41:02 +0200

    added enum class for the classes

---
+++ recaptcha_classifier/labels.py
@@ -0,0 +1,65 @@
+from enum import Enum
+
+
+class ModelClasses(Enum):
+    """
+    Enum for improving readability of the object classes.
+    """
+    CROSSWALK = 0
+    CHIMNEY = 1
+    STAIR = 2
+
+    @classmethod
+    def from_name(cls, name: str) -> int:
+        """
+        Converts a class name to its corresponding integer value.
+
+        Args:
+            name (str): The name of the class.
+
+        Returns:
+            int: The integer value of the class.
+        """
+        upper = name.upper()
+        if upper not in cls.__members__:
+            raise ValueError(f"Class '{name}' does not exist. Please define it"
+                             "into the labels.py file.")
+        return cls[upper].value
+
+    @classmethod
+    def all(cls) -> list:
+        """
+        Returns the list of all classes.
+
+        Returns:
+            list: List of all classes.
+        """
+        return list(cls)
+
+    @classmethod
+    def to_dict(cls) -> dict:
+        """
+        Convert the enum to a dictionary.
+
+        Returns:
+            dict: Dictionary representation of the enum.
+        """
+        return {cl.name: cl.value for cl in cls}
+
+    @classmethod
+    def from_id(cls, id: int) -> str:
+        """
+        Converts an integer ID to its corresponding class name.
+
+        Args:
+            id (int): The ID of the class.
+
+        Returns:
+            str: The name of the class.
+        """
+        for name, member in cls.__members__.items():
+            if member.value == id:
+                return name
+
+        raise ValueError(f"Class with ID '{id} does not exist. Please define "
+                         "it into the labels.py file")
