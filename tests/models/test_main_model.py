6b5fe02a17ddd68ea7296db054824d4cc20adeb0
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-05-13 17:43:42 +0200

    test file added

---
+++ tests/models/test_main_model.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MainModelTest(unittest.TestCase):
+
+    def test_forward(self):
+        pass
+
+    def test_check_args(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
