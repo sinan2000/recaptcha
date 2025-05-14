e06baa9afc5428dcec2278d3a1c1b733a9cff5e5
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-05-14 14:34:48 +0200

    loop part 1

--- recaptcha_classifier/models/main_model/HPoptimizer.py
+++ recaptcha_classifier/models/main_model/HPoptimizer.py
@@ -1,15 +1,21 @@
+import numpy as np
+
+from recaptcha_classifier.models.main_model.model_class import MultiHeadModel
+from recaptcha_classifier.models.training import Trainer
 
 
-class HpOptimizer(object):
+class HPOptimizer(object):
     """ ..."""
 
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, trainer: Trainer):
+        self.trainer = trainer
+        self.opt_data = dict()
 
     def optimize_hyperparameters(self,
                                  n_layers: list = [1, 5],
                                  kernel_sizes: list = [3, 5],
-                                 learning_rates: list = [1e-3, 1e-4]):
+                                 learning_rates: list = [1e-3, 1e-4],
+                                 ):
         """
         Main loop for optimizing hyperparameters.
         :param n_layers: list of integers specifying the number of hidden layers range.
@@ -17,4 +23,27 @@ class HpOptimizer(object):
         :param learning_rates: list of floats specifying the learning rate range.
         :return: model architecture performances ranked from best to worst.
         """
+
+        n = self._check_arg_dims(n_layers, kernel_sizes, learning_rates)
+        layers = np.arange(n_layers, n, dtype=int)
+        kernels = np.arange(kernel_sizes, n, dtype=int)
+        learning_rates = np.arange(learning_rates, n, dtype=float)
+
+        hp = [layers, kernels,learning_rates]
+
+        hp_combos = np.array(np.meshgrid(hp)).T.reshape(-1, len(hp))
+
+        for i in range(len(hp_combos)):
+            hp_combo = hp_combos[i]
+            model = MultiHeadModel(n_layers=int(hp_combo[0]), kernel_size=int(hp_combo[1]))
+            self.trainer.train(model=model, load_checkpoint=False)
+
+
+
+
+                '''name = 'model_' + str(i)
+                self.opt_data[name] = {
+                    'n_layers': layers
+                }
+                self.opt_data[name].append()'''
 
