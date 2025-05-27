74dc7be0fdc4fcb1b53c68639b6ae4c13edf5162
Iveto3 <i.i.ivanova@student.rug.nl>
2025-05-27 20:14:57 +0200

    main.py pipeline draft

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from recaptcha_classifier import (
     DetectionLabels,
-    DataPreprocessingPipeline
+    DataPreprocessingPipeline,
+    SimpleClassifierPipeline
 )
 
-
 def main():
-    data = DataPreprocessingPipeline(
-        DetectionLabels.to_class_map(),
-        balance=True
-        )
+    pipeline = SimpleClassifierPipeline(epochs=1)
+    pipeline.train()
+    pipeline.evaluate()
 
-    loaders = data.run()
 
-    print("Data loaders built successfully.")
+    # data = DataPreprocessingPipeline(
+    #     DetectionLabels.to_class_map(),
+    #     balance=True
+    #     )
 
-    for split, loader in loaders.items():
-        print(f"{split.upper()} DataLoader:")
-        batch = next(iter(loader))
-        images, labels = batch
-        print(f" - images.shape: {images.shape}")
-        print(f" - labels.shape: {labels.shape}")
-        print(f" - class IDs: {labels.tolist()}")
+    # loaders = data.run()
+
+    # print("Data loaders built successfully.")
+
+    # for split, loader in loaders.items():
+    #     print(f"{split.upper()} DataLoader:")
+    #     batch = next(iter(loader))
+    #     images, labels = batch
+    #     print(f" - images.shape: {images.shape}")
+    #     print(f" - labels.shape: {labels.shape}")
+    #     print(f" - class IDs: {labels.tolist()}")
+
 
 
 if __name__ == '__main__':
