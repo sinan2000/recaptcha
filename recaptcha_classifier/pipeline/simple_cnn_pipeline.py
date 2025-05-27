da8d52188e66a37dae6723b4e58d9ba6c9e8a344
Iveto3 <i.i.ivanova@student.rug.nl>
2025-05-27 05:07:17 +0200

    simple_cnn_pipeline draft

--- recaptcha_classifier/pipeline/simple_cnn_pipeline.py
+++ recaptcha_classifier/pipeline/simple_cnn_pipeline.py
@@ -8,6 +8,7 @@ from recaptcha_classifier.models.simple_classifier_model import SimpleCNN
 from recaptcha_classifier.detection_labels import DetectionLabels
 from recaptcha_classifier.data.pipeline import DataPreprocessingPipeline
 from recaptcha_classifier.train.training import Trainer
+from recaptcha_classifier.features.evaluation.evaluate import evaluate_model
 
 
 class SimpleClassifierPipeline:
@@ -33,13 +34,33 @@ class SimpleClassifierPipeline:
         self.scheduler = StepLR(self.optimizer, step_size=step_size, gamma=gamma)
 
         self._trainer = Trainer(train_loader=self.loaders['train'],
-                           val_loader=self.loaders['val'],
-                           epochs=epochs,
-                           optimizer=self.optimizer,
-                           scheduler=self.scheduler,
-                           save_folder=save_folder,
-                           model_file_name=model_file_name,
-                           optimizer_file_name=optimizer_file_name,
-                           scheduler_file_name=scheduler_file_name,
-                           device=device)
+                                val_loader=self.loaders['val'],
+                                epochs=epochs,
+                                optimizer=self.optimizer,
+                                scheduler=self.scheduler,
+                                save_folder=save_folder,
+                                model_file_name=model_file_name,
+                                optimizer_file_name=optimizer_file_name,
+                                scheduler_file_name=scheduler_file_name,
+                                device=device)
 
+        # ????????
+        self._load_checkpoint = (
+            os.path.exists(os.path.join(save_folder, model_file_name)) and
+            os.path.exists(os.path.join(save_folder, optimizer_file_name)) and
+            os.path.exists(os.path.join(save_folder, scheduler_file_name))
+        )
+
+    def train(self) -> None:
+        self._trainer.train(self._model, self._load_checkpoint)
+
+    def evaluate(self, plot_cm: bool = False) -> dict:
+        eval_results = evaluate_model(
+            model=self._model,
+            test_loader=self._loaders['test'],
+            device=self._trainer.device,
+            num_classes=len(self._class_map),
+            class_names=list(self._class_map.keys()),
+            plot_cm=plot_cm
+        )
+        return eval_results
