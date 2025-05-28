1922f75c352cfa873d395a32ab48ed235ecc8c48
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-05-28 16:15:13 +0200

    kfold cv now works with dataloaders

--- recaptcha_classifier/models/main_model/kfold_validation.py
+++ recaptcha_classifier/models/main_model/kfold_validation.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import KFold
 from sympy.printing.pytorch import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
+
+from recaptcha_classifier import DetectionLabels
 from recaptcha_classifier.features.evaluation.evaluate import evaluate_model
 from recaptcha_classifier.models.main_model.HPoptimizer import HPOptimizer
 from recaptcha_classifier.models.main_model.model_class import MainCNN
@@ -13,7 +15,10 @@ class KFoldValidation:
     integrated with hyperparameter optimization.
     """
 
-    def __init__(self, data, k_folds: int,
+    def __init__(self,
+                 train_loader: DataLoader,
+                 val_loader: DataLoader,
+                 k_folds: int,
                  hp_optimizer: HPOptimizer,
                  device=None) -> None:
         """
@@ -24,7 +29,9 @@ class KFoldValidation:
         :param hp_optimizer: Instance of HPOptimizer
         :param device: Optional torch device
         """
-        self.data = data
+        self._class_map = DetectionLabels.all() # class labels
+        self.train_loader = train_loader
+        self.val_loader = val_loader
         self.k_folds = k_folds
         self.hp_optimizer = hp_optimizer
         self.device = device
@@ -32,32 +39,41 @@ class KFoldValidation:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self._best_models = pd.DataFrame()
 
+
     def run_cross_validation(self,
-                             save_checkpoints: bool = True) -> None:
+                             save_checkpoints: bool = True,
+                             load_checkpoints: bool = False,
+                             batch_size: int = 32) -> None:
         """
         Runs k-Fold Cross-Validation and hyperparameter optimization.
 
+        :param batch_size: size of batches in fold data loaders
         :param save_checkpoints: boolean flag to save checkpoints
-        :param top_n_models: Number of best models to keep from each fold.
         """
-        kf = KFold(n_splits=self.k_folds, shuffle=True, random_state=42)
-        class_names = ['BICYCLE', 'BRIDGE', 'BUS', 'CAR', 'CHIMNEY',
-                       'CROSSWALK', 'HYDRANT', 'MOTORCYCLE', 'PALM',
-                       'STAIR', 'TRAFFIC_LIGHT', 'OTHER']
 
-        for fold_index, (train_indices, val_indices) \
-                in enumerate(kf.split(self.data)):
+        # Concatenate all data indices from both loaders
+        train_indices = list(range(len(self.train_loader.dataset)))
+        val_indices = list(range(len(self.val_loader.dataset)))
+        all_indices = train_indices + val_indices
+        dataset = self.train_loader.dataset  # Assumes train and val use the same type of Dataset
+
+        kf = KFold(n_splits=self.k_folds, shuffle=True, random_state=42)
+
+        for fold_index, (train_idx, val_idx) in enumerate(kf.split(all_indices)):
             print(f"\n--- Fold {fold_index + 1}/{self.k_folds} ---")
 
-            train_data = [self.data[i] for i in train_indices]
-            val_data = [self.data[i] for i in val_indices]
+            # Create Subsets for this fold
+            train_subset = Subset(dataset, [all_indices[i] for i in train_idx])
+            val_subset = Subset(dataset, [all_indices[i] for i in val_idx])
 
-            train_loader = DataLoader(train_data, batch_size=32)
-            val_loader = DataLoader(val_data, batch_size=32)
+            # Create DataLoaders for this fold
+            fold_train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=False)
+            fold_val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
 
-            self.hp_optimizer._trainer.train_loader = train_loader
-            self.hp_optimizer._trainer.val_loader = val_loader
+            self.hp_optimizer.trainer.train_loader = fold_train_loader
+            self.hp_optimizer.trainer.val_loader = fold_val_loader
 
+            # --- Train and evaluate your model here ---
             optimized_hp_dataframe = self.hp_optimizer.optimize_hyperparameters(save_checkpoints=save_checkpoints)
             evaluated_models = {'Accuracy': [],
                                 'F1-score': [],
@@ -67,11 +83,14 @@ class KFoldValidation:
                 model = MainCNN(n_layers=int(row['layers']),
                                 kernel_size=int(row['kernel_sizes']),
                                 num_classes=12)
+                self.hp_optimizer.trainer.train(model=model,
+                                                save_checkpoint=save_checkpoints,
+                                                load_checkpoint=load_checkpoints)
                 metrics_result = evaluate_model(
-                    model, val_loader, device=self.device, num_classes=12,
-                    class_names=class_names, plot_cm=False
+                    model, fold_val_loader, device=self.device, num_classes=12,
+                    class_names=self._class_map, plot_cm=False
                 )
-                # removing confusion matrix to keep dimensions balanced
+                # removing confusion matrix to keep df dimensions balanced
                 metrics_result.pop('Confusion Matrix')
                 metrics_result = pd.DataFrame(metrics_result, index=[0])
                 evaluated_models = pd.concat([evaluated_models, metrics_result])
