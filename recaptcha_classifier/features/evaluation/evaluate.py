5f359356713f2741339ad0c0b2f537ad23c83496
Iveto3 <i.i.ivanova@student.rug.nl>
2025-05-29 16:12:51 +0200

    evaluate.py flake8

--- recaptcha_classifier/features/evaluation/evaluate.py
+++ recaptcha_classifier/features/evaluation/evaluate.py
@@ -53,7 +53,6 @@ def evaluate_model(model: torch.nn.Module,
     )
     results.update(class_results)
 
-    # Print results
     print("\n--- Evaluation Results ---")
     for key, val in results.items():
         if isinstance(val, torch.Tensor):
