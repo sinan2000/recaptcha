47b5dc9e870214d130f83781d8078fa2071dff70
Iveto3 <i.i.ivanova@student.rug.nl>
2025-05-29 16:43:12 +0200

    main.py flake8

--- main.py
+++ main.py
@@ -4,34 +4,12 @@ from recaptcha_classifier.pipeline.main_model_pipeline import (
     MainModelPipeline)
 
 
-# from recaptcha_classifier import (
-#     DetectionLabels,
-#     DataPreprocessingPipeline,
-#     SimpleClassifierPipeline
-# )
-
 def main():
     pipeline1 = SimpleClassifierPipeline(epochs=1)
     pipeline1.run()
 
     pipeline2 = MainModelPipeline(epochs=1)
     pipeline2.run()
-    # data = DataPreprocessingPipeline(
-    #     DetectionLabels.to_class_map(),
-    #     balance=True
-    #     )
-
-    # loaders = data.run()
-
-    # print("Data loaders built successfully.")
-
-    # for split, loader in loaders.items():
-    #     print(f"{split.upper()} DataLoader:")
-    #     batch = next(iter(loader))
-    #     images, labels = batch
-    #     print(f" - images.shape: {images.shape}")
-    #     print(f" - labels.shape: {labels.shape}")
-    #     print(f" - class IDs: {labels.tolist()}")
 
 
 if __name__ == '__main__':
