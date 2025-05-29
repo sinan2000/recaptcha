69ba6a35394a1f65d0f40b6f69628544b4971fad
Iveto3 <i.i.ivanova@student.rug.nl>
2025-05-29 16:26:39 +0200

    main_model_pipeline flake8

--- recaptcha_classifier/pipeline/main_model_pipeline.py
+++ recaptcha_classifier/pipeline/main_model_pipeline.py
@@ -9,6 +9,7 @@ from recaptcha_classifier.pipeline import BasePipeline
 
 
 class SimpleClassifierPipeline(BasePipeline):
+    """Pipeline for training a simple classifier model."""
     def __init__(self,
                  step_size: int = 5,
                  gamma: float = 0.5,
@@ -19,7 +20,33 @@ class SimpleClassifierPipeline(BasePipeline):
                  model_file_name: str = "model.pt",
                  optimizer_file_name: str = "optimizer.pt",
                  scheduler_file_name: str = "scheduler.pt"
-                 ):
+                 ) -> None:
+        """
+        Constructor for SimpleClassifierPipeline class.
+
+        Args:
+            step_size (int, optional): Step size for the learning rate
+                scheduler. Defaults to 5.
+            gamma (float, optional): Gamma value for the learning rate
+                scheduler. Defaults to 0.5.
+            lr (float, optional): Learning rate for the optimizer.
+                Defaults to 0.001.
+            epochs (int, optional): Number of epochs for training.
+                Defaults to 20.
+            device (torch.device, optional): Device for training.
+                Defaults to None.
+            save_folder (str, optional): Folder for saving checkpoint files.
+                Defaults to "".
+            model_file_name (str, optional): Name of the model checkpoint
+                file. Defaults to "model.pt".
+            optimizer_file_name (str, optional): Name of the optimizer
+                checkpoint file. Defaults to "optimizer.pt".
+            scheduler_file_name (str, optional): Name of the scheduler
+                checkpoint file. Defaults to "scheduler.pt".
+
+        Returns:
+            None
+        """
         super().__init__(step_size, gamma, lr, epochs, device,
                          save_folder, model_file_name,
                          optimizer_file_name, scheduler_file_name)
@@ -27,24 +54,38 @@ class SimpleClassifierPipeline(BasePipeline):
         self._hp_optimizer = None
 
     def run(self) -> None:
+        """ Runs the pipeline."""
         self.data_loader()
         self._trainer = self._initialize_trainer(self._model)
         self._hp_optimizer = HPOptimizer(trainer=self._trainer)
 
-        # model gets initialized inside here:
+        # the best model gets initialized inside here
         self._run_kfold_cross_validation()
 
         self._trainer.train()
         self.evaluate()
 
     def data_loader(self) -> None:
+        """Loads the data."""
         super().data_loader()
 
     def _run_kfold_cross_validation(self,
                                     k_folds: int = 5,
                                     n_layers: int = 3,
                                     kernel_size: int = 5) -> None:
+        """
+        Runs k-fold cross validation.
 
+        Args:
+            k_folds (int, optional): Number of folds. Defaults to 5.
+            n_layers (int, optional): Number of layers in the model.
+                Defaults to 3.
+            kernel_size (int, optional): Kernel size for the model.
+                Defaults to 5.
+
+        Returns:
+            None
+        """
         self._kfold = KFoldValidation(
             data=self._data,
             k_folds=self.k_folds,
